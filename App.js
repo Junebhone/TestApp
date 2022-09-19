1d8a1b23ca090399858e686c5f73e40b39ab72d1
Junebhone <bhonekyawsihein@gmail.com>
2022-09-19 14:09:37 +0630

    second commit

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import {SafeAreaView, View, Text, Image} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
+import Card from './components/Card';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Cash In',
+    image: require('./images/cashIn.png'),
+  },
+  {
+    id: '2',
+    title: 'Cash Out',
+    image: require('./images/cashOut.png'),
+  },
+];
+
+const otherFunctions = [
+  {
+    id: '1',
+    title: 'Bill Payment',
+    image: require('./images/bill.png'),
+  },
+  {
+    id: '2',
+    title: 'Transfer',
+    image: require('./images/transfer.png'),
+  },
+  {
+    id: '3',
+    title: 'History',
+    image: require('./images/history.png'),
+  },
+  {
+    id: '4',
+    title: 'Account Opening',
+    image: require('./images/accountOpening.png'),
+  },
+];
 
 const App = () => {
+  const renderItem = ({item}) => <Card title={item.title} image={item.image} />;
   return (
-    <SafeAreaView className="flex-1 bg-[#F3F5FF]">
+    <View className="flex-1 bg-[#F3F5FF] ">
       <View className="flex  flex-row justify-between p-[16px]">
         <View className="flex flex-row">
           <Image
@@ -20,67 +58,108 @@ const App = () => {
             className="w-4 h-4"></Image>
         </View>
       </View>
-
-      <View className="mx-4 gap-y-2">
-        <View className="bg-white rounded-md divide-y divide-gray-100">
-          <View className="px-4 py-2 flex flex-col gap-2">
-            <Text className="text-[18px] opacity-30">Total Amount</Text>
-            <Text className="text-[28px] font-bold color-[#47980E]">
-              250,000 MMK
-            </Text>
-          </View>
-          <View className="flex flex-row justify-between px-4 py-2">
-            <View className="flex flex-col gap-2">
-              <Text className="text-[18px] opacity-30">e-Pension</Text>
-              <Text className="text-[20px] font-bold">09421768835</Text>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View className="mx-4 gap-y-2">
+          <View className="bg-white rounded-md divide-y divide-gray-100">
+            <View className="px-4 py-2 flex flex-col gap-2">
+              <Text className="text-[18px] opacity-30">Total Amount</Text>
+              <Text className="text-[28px] font-bold color-[#47980E]">
+                250,000 MMK
+              </Text>
             </View>
-            <View className="flex flex-col gap-2">
-              <Text className="text-[18px] opacity-30">MMK</Text>
-              <Text className="text-[20px] font-bold color-[#47980E]">
-                250,000
+            <View className="flex flex-row justify-between px-4 py-2">
+              <View className="flex flex-col gap-2">
+                <Text className="text-[18px] opacity-30">e-Pension</Text>
+                <Text className="text-[20px] font-bold">09421768835</Text>
+              </View>
+              <View className="flex flex-col gap-2">
+                <Text className="text-[18px] opacity-30">MMK</Text>
+                <Text className="text-xl font-bold color-[#47980E]">
+                  250,000
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View className="mx-[12px]">
+          <View className="flex flex-row">
+            {DATA.map(item => {
+              return (
+                <Card
+                  key={item.id}
+                  title={item.title}
+                  image={item.image}></Card>
+              );
+            })}
+          </View>
+        </View>
+        <View className="mx-[12px]">
+          <Text className="font-bold text-xl">Other Functions</Text>
+          <View className="flex flex-row flex-wrap ">
+            {otherFunctions.map(item => {
+              return (
+                <Card
+                  key={item.id}
+                  title={item.title}
+                  image={item.image}></Card>
+              );
+            })}
+          </View>
+        </View>
+        <View className="mx-[16px]">
+          <Text className="font-bold text-xl mb-4">News</Text>
+          <View className="flex flex-row bg-white rounded-md justify-center items-center px-4 py-2 gap-2">
+            <View className="">
+              <Image
+                source={require('./images/news.png')}
+                className="w-[60px] h-[60px]"></Image>
+            </View>
+            <View className="flex flex-col flex-wrap">
+              <Text>Invitation For Bids</Text>
+              <Text className="flex-wrap w-[280px]">
+                The Ministry of Planning, Finance and Industry invites bids for
+                Upgrading Mini...
               </Text>
             </View>
           </View>
         </View>
-        <View className="flex flex-row gap-x-2">
-          <View className="bg-white p-2 rounded-md flex-1">
-            <View className="flex flex-row justify-start">
-              <View className="bg-gray-100 rounded-full flex justify-center items-center w-12 h-12">
-                <Image
-                  source={require('./images/cashIn.png')}
-                  className=" w-8 h-8"></Image>
-              </View>
-            </View>
-            <View className="flex flex-row justify-end">
-              <Text className="flex justift-end font-bold text-lg">
-                Cash In
-              </Text>
-            </View>
+        <View style={{height: 100, backgroundColor: 'red'}}></View>
+      </ScrollView>
+      <View className="flex-1 flex-row justify-center items-center gap-x-10 bg-white">
+        <View className="flex flex-col">
+          <View>
+            <Image
+              source={require('./images/scan.png')}
+              className="w-[26px] h-[26px]"></Image>
+            <Text>Scan</Text>
           </View>
-          <View className="bg-white p-2 rounded-md flex-1">
-            <View className="flex flex-row justify-start">
-              <View className="bg-gray-100 rounded-full flex justify-center items-center w-12 h-12">
-                <Image
-                  source={require('./images/cashIn.png')}
-                  className=" w-8 h-8"></Image>
-              </View>
-            </View>
-            <View className="flex flex-row justify-end">
-              <Text className="flex justift-end font-bold text-lg">
-                Cash Out
-              </Text>
-            </View>
+        </View>
+        <View className="flex flex-col">
+          <View>
+            <Image
+              source={require('./images/qr.png')}
+              className="w-[26px] h-[26px]"></Image>
+            <Text>Scan</Text>
+          </View>
+        </View>
+        <View className="flex flex-col">
+          <View>
+            <Image
+              source={require('./images/topUp.png')}
+              className="w-[26px] h-[26px]"></Image>
+            <Text>Scan</Text>
+          </View>
+        </View>
+        <View className="flex flex-col">
+          <View>
+            <Image
+              source={require('./images/menu.png')}
+              className="w-[27px] h-[16px]"></Image>
+            <Text>Scan</Text>
           </View>
         </View>
       </View>
-      <View className="flex-1 justify-end ">
-        <View className="bg-white">
-          <Image
-            source={require('./images/scan.png')}
-            className="w-[26px] h-[26px]"></Image>
-        </View>
-      </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
 
-const Card = () => {
-  return (
-    <View className="bg-white rounded-md p-2 w-1/2">
-      <View className="flex justify-start">
-        <View className="bg-gray-100 rounded-full flex justify-center items-center w-12 h-12">
-          <Image
-            source={require('../images/cashIn.png')}
-            className=" w-8 h-8 opacity-60"></Image>
-        </View>
-      </View>
-      <View className="flex justify-end">
-        <Text className="flex justift-end">Cash In</Text>
+
+const Card = ({title, image}) => (
+  <View className=" bg-white p-2 m-[4px] rounded-md flex-1 w-[45%]">
+    <View className="flex flex-row justify-start">
+      <View className="bg-gray-100 rounded-full flex justify-center items-center w-12 h-12">
+        <Image source={image} className=" w-8 h-8"></Image>
       </View>
     </View>
-  );
-};
-
+    <View className="flex flex-row justify-end">
+      <Text className="flex justift-end font-bold text-lg">{title}</Text>
+    </View>
+  </View>
+);
 export default Card;
